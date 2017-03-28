cd07a71cbd793238a69fd8b62d16b804e6470fb8
glass eagle <mario.torres2@yahoo.com>
2017-03-28 06:38:14 +0000

    imagesearchHandler.server.js: request search from pixabay and send raw Data

--- controllers/imagesearchHandler.server.js
+++ controllers/imagesearchHandler.server.js
@@ -1,7 +1,33 @@
 "use strict";
 
+var config = require(process.env.HOME + "/secret/config.js");
+var pixabay_api_key = config.pixabay_api_key;
+var https = require("https");
+
 function imagesearchHandler(req, res){
-  res.send(req.params.query + "?offset=" + req.query.offset)
+    var hostname = "pixabay.com";
+    var path = "/api/?" +
+        "key=" + pixabay_api_key +
+        "&q=" + req.params.query +
+        "&per_page=10" +
+        "&page=" + req.query.offset;
+        
+    var options = {"hostname": hostname, "path": path};
+    https.get(options, pixabay_callback);
+  
+    function pixabay_callback(pixabay_res){
+        let rawData = "";
+        
+        pixabay_res.on("data", function(chunk){
+            rawData+=chunk;
+        });
+        
+        pixabay_res.on("end", function(){
+            res.send(rawData);
+        });
+    }  
 }
+
+
 
 module.exports = imagesearchHandler;
