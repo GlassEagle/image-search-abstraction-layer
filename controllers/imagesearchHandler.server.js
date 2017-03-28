739225b6f95f97d1cb7a375cb4de9bb4be54f0ab
glass eagle <mario.torres2@yahoo.com>
2017-03-28 08:46:59 +0000

    parse and send pixabay search results

--- controllers/imagesearchHandler.server.js
+++ controllers/imagesearchHandler.server.js
@@ -23,12 +23,29 @@ function imagesearchHandler(req, res){
         });
         
         pixabay_res.on("end", function(){
+            var toSend = parse_pixabay_results(rawData);
+            toSend = JSON.stringify(toSend);
             res.setHeader("content-type", "application/json");
-            res.send(rawData);
+            res.send(toSend);
         });
     }  
 }
 
-
+function parse_pixabay_results(json){
+    var data = JSON.parse(json);
+    var hits = [];
+    var index;
+    
+    for (index in data.hits){
+        let obj = {};
+        obj.url = data.hits[index].webformatURL;
+        obj.snippet = data.hits[index].tags;
+        obj.thumbnail = data.hits[index].previewURL;
+        obj.context = data.hits[index].pageURL;
+        hits.push(obj);
+    }
+    
+    return hits;
+}
 
 module.exports = imagesearchHandler;
