820ba16020dba88da3b168d57a50016e942e5121
glass eagle <mario.torres2@yahoo.com>
2017-04-04 06:58:23 +0000

    query db for recent searches and send results

--- controllers/latestimagesearchHandler.server.js
+++ controllers/latestimagesearchHandler.server.js
@@ -1,7 +1,21 @@
-"use strict"
+"use strict";
+
+var recentSearch = require("../models/recentSearches.js");
 
 function latestimagesearchHandler(req, res){
-    res.send("latestimagesearchHandler")
+    recentSearch
+    .find({}, {_id: false, term: true, when: true})
+    .limit(10)
+    .sort("-when")
+    .exec(function(err, doc){
+        if(err){
+            res.setHeader("content-type", "application/json");
+            res.send(JSON.stringify(err));
+            return;
+        }
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(doc));
+    });
 }
 
-module.exports = latestimagesearchHandler
+module.exports = latestimagesearchHandler;
