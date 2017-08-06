4df278ab8e1cec4b6e570bc08bd3d5df2fc1eba5
glass eagle <mario.torres2@yahoo.com>
2017-08-06 08:16:10 +0000

    added page at /

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost:27017/data");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + "/public/index.html");
+});
 
 app.get("/api/imagesearch/:query", imagescearchHandler)
 app.get("/api/latest/imagesearch", latestimagesearchHandler)
